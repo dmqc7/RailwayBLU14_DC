4f6b9a6db5dcc68bb3a9b14f4022e5db7e57f6ad
dmqc7 <duarte.correia@sapo.pt>
2023-04-01 17:23:06 +0100

    railwayDeploy_protectedserver_fix2

--- protected_server.py
+++ protected_server.py
@@ -235,4 +235,5 @@ def update():
 
     
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000)) # Use port specified by environment variable, or default to 5000
+    app.run(host='0.0.0.0', port=port)
